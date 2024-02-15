7e7a47accf9db337a9bd420363b084ef122f697b
SeungMinJeon <100692591+HermitCrapOfLine@users.noreply.github.com>
2024-02-15 19:25:54 +0900

    24.02.15 Java Study

---
+++ EssentialOfJava/JavaStudy/src/Chapter10/DecimalFormat/DecimalFormat2.java
@@ -0,0 +1,20 @@
+package Chapter10.DecimalFormat;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormat2 {
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("#,###.##");
+		DecimalFormat df2 = new DecimalFormat("#.###E0");
+		
+		try {
+			Number num = df.parse("1,234,567.89");
+			System.out.print("1,234,567.89" + " -> ");
+			
+			double d = num.doubleValue();
+			System.out.print(d + " -> ");
+			
+			System.out.print(df2.format(num));
+		} catch(Exception e) {}
+	}
+}
