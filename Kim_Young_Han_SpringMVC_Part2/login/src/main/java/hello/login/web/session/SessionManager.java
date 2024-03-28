94239cfe18c6a1a1618b757c194e6df4c618565f
SeungMinJeon <100692591+HermitCrapOfLine@users.noreply.github.com>
2024-03-28 09:20:53 +0900

    로그인 처리 공부

---
+++ Kim_Young_Han_SpringMVC_Part2/login/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,63 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    private static final String SESSION_COOKIE_NAME = "mySessionId";
+
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>(); // 동시성 문제 해결
+
+    /*
+    * 세션 생성
+    * */
+
+    public void createSession (Object value, HttpServletResponse response) {
+
+        // 세션 id를 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        // 쿠키 생성
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+
+    }
+
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies == null) {
+            return null;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(SESSION_COOKIE_NAME)) {
+                return sessionStore.get(cookie.getValue());
+            }
+        }
+        return null;
+    }
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName) {
+
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(cookieName))
+                .findFirst()
+                .orElse(null);
+    }
+}
